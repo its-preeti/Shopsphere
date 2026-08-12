b8f2ddf5dfa0af9eee50467e9f21e5a9f4686ca0
Preeti Desale <desalepreeti@gmail.com>
2026-08-12 22:43:56 +0530

    Fix CORS for Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://shopsphere-drab.vercel.app/",
+      "https://shopsphere-drab.vercel.app",
     ],
     credentials: true,
   })
